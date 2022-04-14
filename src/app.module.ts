9c78fae4879a3b9dce7b3803caa51298c50a3f6d
davicn <davi.nascimento777@gmail.com>
2022-04-14 14:46:32 -0300

    config inicial do mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DoctorsModule } from './doctors/doctors.module';
 import { PatientsModule } from './patients/patients.module';
 
 @Module({
-  imports: [DoctorsModule, PatientsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://oministack10:<password>@cluster0.nizan.mongodb.net/test'),
+    DoctorsModule,
+    PatientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/doctors/dto/create-doctor.dto.ts
+++ src/doctors/dto/create-doctor.dto.ts
@@ -1,1 +1,8 @@
-export class CreateDoctorDto {}
+export class CreateDoctorDto {
+    nome: string;
+    crm: string;
+    email: string;
+    password: string;
+    endereco: string;
+    pacientes: Array<string>
+}
