81f9b2785e00fdae8725f7a3b3faf6ed8dcffb6b
Davi Nascimento <davi.nascimento777@gmail.com>
2022-04-15 11:26:17 -0300

    Correção em testes e mudanças em swagger

--- src/doctors/dto/create-doctor.dto.ts
+++ src/doctors/dto/create-doctor.dto.ts
@@ -1,8 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateDoctorDto {
+    @ApiProperty()
     nome: string;
+    @ApiProperty()
     crm: string;
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     password: string;
+    @ApiProperty()
     endereco: string;
+    @ApiProperty()
     pacientes: Array<string>
 }
--- src/doctors/entities/doctor.entity.ts
+++ src/doctors/entities/doctor.entity.ts
@@ -1,38 +1,27 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { ApiProperty } from '@nestjs/swagger';
 
 
 export type DoctorDocument = Doctor & Document;
 
 @Schema()
 export class Doctor {
-    @ApiProperty({
-        description: 'Nome do médico',
-        type: String
-    })
-
-    @ApiProperty()
+   
     @Prop()
     nome: string;
 
-    @ApiProperty()
     @Prop()
     crm: string;
 
-    @ApiProperty()
     @Prop()
     email: string;
 
-    @ApiProperty()
     @Prop()
     password: string;
 
-    @ApiProperty()
     @Prop()
     endereco: string;
 
-    @ApiProperty()
     @Prop()
     pacientes: Array<string>
 }
